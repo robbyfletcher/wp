67a1e3cd3a879819e49a6adc9ec1dc952a2868b6
Robby Fletcher <robbyf114@gmail.com>
2016-11-02 21:22:40 -0500

    Added some more functionality

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-// var schedule = require('node-schedule');
-
-// var rule = new schedule.RecurrenceRule();
-
-// rule.second = Array.from({length: 30}, (v, k) => k*2);
-
-// var j = schedule.scheduleJob(rule, function() {
-// 	console.log('2 SECONDS!\n');
-// });
-
+var sqlite3 = require('sqlite3').verbose();
+var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
+var wallpaper = require('wallpaper');
+
+var db = new sqlite3.Database('wp.db');
+
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    // console.log('content-type:', res.headers['content-type']);
+    // console.log('content-length:', res.headers['content-length']);
+    if(res.headers['content-type'] === 'image/jpeg') {
+    	request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+    }
+  });
+};
+
+var intersect = function(a, b, key) {
+	var atmp = a.map(function(e) {return e[key]; });
+	var btmp = b.map(function(e) {return e[key]; });
+	return atmp.filter(function(e) {return btmp.indexOf(e) != -1; });
+ };
 
 request('https://www.reddit.com/r/EarthPorn/search?q=1920x1080&sort=top&restrict_sr=on&t=all',
 	function (error, response, body) {
@@ -22,9 +33,42 @@ request('https://www.reddit.com/r/EarthPorn/search?q=1920x1080&sort=top&restrict
 					var obj = new Object;
 					obj.title = $(this).find('.search-result-header').text();
 					obj.link = $(this).find('.search-result-footer').text();
+					if (!obj.link.endsWith('.jpg')) { obj.link += '.jpg' }
 					arr.push(obj);
 				}
 			});
-			console.log(arr);
+
+			db.run('CREATE TABLE IF NOT EXISTS wp (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, link TEXT, \
+				UNIQUE(title, link))', function(err) {
+				if (err) {
+					console.error(err);
+				}
+
+				// var stmt = db.prepare('SELECT * FROM wp WHERE link = ($link)');
+				var stmt = db.prepare('INSERT OR IGNORE INTO wp (title, link) VALUES ($title, $link)');
+
+				arr.forEach(function (e, i) {
+					stmt.run({
+						$title: e.title,
+						$link: e.link
+					}, function (err, res) {
+							if (err) {
+								console.error(err);
+							}
+							if (this.changes) {
+								download(e.link, 'wallpapers/' + this.lastID + '.jpeg', function(e) {});
+							}
+					});
+				});
+			});
+
+			db.get('SELECT * FROM wp ORDER BY RANDOM() LIMIT 1', function(err, res) {
+				if (err) {
+					console.error(err);
+				}
+				wallpaper.set('wallpapers/' + res.id + '.jpeg').then(() => {
+					console.log('Wallpaper Set: ' + res.title);
+				});
+			});
 		}
 	});
